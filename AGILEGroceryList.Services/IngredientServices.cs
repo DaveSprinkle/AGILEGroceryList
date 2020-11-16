559ba44aacbc931eae3c14808a8416540e0b7525
Dave Sprinkle <davesprinkle3@gmail.com>
2020-11-16 14:45:41 -0500

    Ingredients add created

---
+++ AGILEGroceryList.Services/IngredientServices.cs
@@ -0,0 +1,41 @@
+﻿using AGILEGroceryList.Data;
+using AGILEGroceryList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGILEGroceryList.Services
+{
+    public class IngredientServices
+    {
+
+
+		private readonly Guid _userId;
+
+		public IngredientServices(Guid userId)
+		{
+			_userId = userId;
+		}
+
+
+
+
+		public bool IngredientCreate(CreateIngredient model)
+		{
+			var entity =
+				new Ingredient
+				{
+					Name = model.Name,
+				};
+
+			using (var ctx = new ApplicationDbContext())
+			{
+				ctx.Ingredients.Add(entity);
+				return ctx.SaveChanges() == 1;
+			}
+		}
+
+    }
+}
