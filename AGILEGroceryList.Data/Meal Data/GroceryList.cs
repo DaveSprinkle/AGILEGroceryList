8008ac5b13ab5998aafa71ba476bf1f464b8e104
sreskala <sam.sagas@hotmail.com>
2020-11-16 14:47:36 -0500

    Sam Reskala - Ticket 6 - Added Grocery List data

--- AGILEGroceryList.Data/Meal Data/GroceryList.cs
+++ AGILEGroceryList.Data/Meal Data/GroceryList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace AGILEGroceryList.Data
 {
     public class GroceryList
     {
+        [Key]
+        public int GroceryListId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
     }
 }
