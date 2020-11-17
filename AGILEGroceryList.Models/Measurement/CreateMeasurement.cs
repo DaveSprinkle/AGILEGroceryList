9d7163fb31d45940be1eb914cac7ea9979232cc9
Karl Ehlhardt <ehlhardt_karl@gmail.com>
2020-11-17 15:20:37 -0500

    break

---
+++ AGILEGroceryList.Models/Measurement/CreateMeasurement.cs
@@ -0,0 +1,16 @@
+﻿using AGILEGroceryList.Data.Measurements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGILEGroceryList.Models.Measurement
+{
+    public class CreateMeasurement
+    {
+        public string Name { get; set; }//Cups or grams
+        public string TypeOfMeasurementString { get; set; }//Volume / Mass / Quanity
+        public double Conversion { get; set; }// from basic number to this value x*Conversion = Basic Storage Value | BasicStorageValue* Conversion = Valuse for grocery list
+    }
+}
---
+++ AGILEGroceryList.Services/MeasurementService.cs
@@ -0,0 +1,37 @@
+﻿using AGILEGroceryList.Data;
+using AGILEGroceryList.Data.Measurements;
+using AGILEGroceryList.Models;
+using AGILEGroceryList.Models.Ingredient;
+using AGILEGroceryList.Models.Measurement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AGILEGroceryList.Services
+{
+    public class MeasurementService
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+
+        //==========================CREATE===============================//
+
+        public async Task<bool> CreateMeasurementList(CreateMeasurement model)
+        {
+            var entity =
+                new Measurement()
+                {
+                    Name = model.Name,
+                    TypeOfMeasurement = (TypeOfMeasurement) Enum.Parse(typeof(TypeOfMeasurement), model.TypeOfMeasurementString),
+                    Conversion = model.Conversion
+                };
+
+            _context.Measurements.Add(entity);
+            return await _context.SaveChangesAsync() == 1;
+        }
+
+    }
+}
